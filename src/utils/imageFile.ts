2417a340f4f5804e46e6a2b7f93e9ff84b41ad7a
tuannguyen <ndtuan.bkdn@gmail.com>
2022-09-26 21:43:58 +0700

    fix error file

--- src/utils/imageFile.ts
+++ src/utils/imageFile.ts
@@ -6,29 +6,33 @@ const ImageFile = async (
   width: number,
   height: number
 ): Promise<string> => {
-  const url = `../UdacityImage/images/full/${filename}.jpg`;
-  const imgName = `${filename}_${width}_${height}.jpg`;
-  const saveFile = `../UdacityImage/images/temporary/${imgName}`;
+  try {
+    const url = `../UdacityImage/images/full/${filename}.jpg`;
+    const imgName = `${filename}_${width}_${height}.jpg`;
+    const saveFile = `../UdacityImage/images/temporary/${imgName}`;
 
-  if (!fs.existsSync(url)) {
-    return '';
-  }
+    if (!fs.existsSync(url)) {
+      return '';
+    }
 
-  if (!fs.existsSync('../UdacityImage/images/temporary')) {
-    fs.mkdirSync('../UdacityImage/images/temporary');
-  }
+    if (!fs.existsSync('../UdacityImage/images/temporary')) {
+      fs.mkdirSync('../UdacityImage/images/temporary');
+    }
 
-  if (!fs.existsSync(saveFile)) {
-    await sharp(url)
-      .resize({
-        width,
-        height
-      })
-      .toFile(saveFile);
-  }
-  return `
+    if (!fs.existsSync(saveFile)) {
+      await sharp(url)
+        .resize({
+          width,
+          height
+        })
+        .toFile(saveFile);
+    }
+    return `
 <img src="../../images/temporary/${imgName}" alt="Image Resize">
 `;
+  } catch (e) {
+    return 'Image can not process';
+  }
 };
 
 export default ImageFile;
